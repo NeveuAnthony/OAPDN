6a698a5529fa09b66fb5a0f165a3e255029f7e78
rhabban <corentin.chupin@gmail.com>
2015-10-20 12:05:26 +0200

    snowBall v1.1

--- app/cache/dev/twig/b/9/b954842641b283ce614946f73d24a16df0647157d73f7f3c4ca889b10cb4e429.php
+++ app/cache/dev/twig/b/9/b954842641b283ce614946f73d24a16df0647157d73f7f3c4ca889b10cb4e429.php
@@ -22,20 +22,20 @@ class __TwigTemplate_49939be530bbca10722d1508177345cd4c83d1a9fa808c58c8c537b5989
 
     protected function doDisplay(array $context, array $blocks = array())
     {
-        $__internal_0be9976c24cedbba91b876ada1d764987491cd64778d8011b00aa8df3fdd5f2f = $this->env->getExtension("native_profiler");
-        $__internal_0be9976c24cedbba91b876ada1d764987491cd64778d8011b00aa8df3fdd5f2f->enter($__internal_0be9976c24cedbba91b876ada1d764987491cd64778d8011b00aa8df3fdd5f2f_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "snowballBundle:Default:index.html.twig"));
+        $__internal_80f72a3d937f7142eab94d17b6f72f145492c4e315e9d7e80d640d887cccaee1 = $this->env->getExtension("native_profiler");
+        $__internal_80f72a3d937f7142eab94d17b6f72f145492c4e315e9d7e80d640d887cccaee1->enter($__internal_80f72a3d937f7142eab94d17b6f72f145492c4e315e9d7e80d640d887cccaee1_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "snowballBundle:Default:index.html.twig"));
 
         $this->parent->display($context, array_merge($this->blocks, $blocks));
         
-        $__internal_0be9976c24cedbba91b876ada1d764987491cd64778d8011b00aa8df3fdd5f2f->leave($__internal_0be9976c24cedbba91b876ada1d764987491cd64778d8011b00aa8df3fdd5f2f_prof);
+        $__internal_80f72a3d937f7142eab94d17b6f72f145492c4e315e9d7e80d640d887cccaee1->leave($__internal_80f72a3d937f7142eab94d17b6f72f145492c4e315e9d7e80d640d887cccaee1_prof);
 
     }
 
     // line 3
     public function block_body($context, array $blocks = array())
     {
-        $__internal_dbcb598e1f16a376a033a469bc6fe1f94cf257cb2862e2d2d2e7b06a6993e05e = $this->env->getExtension("native_profiler");
-        $__internal_dbcb598e1f16a376a033a469bc6fe1f94cf257cb2862e2d2d2e7b06a6993e05e->enter($__internal_dbcb598e1f16a376a033a469bc6fe1f94cf257cb2862e2d2d2e7b06a6993e05e_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "body"));
+        $__internal_fa49966e1e83f5a176868fbf2fa85e1865ecfed4ea5b0dc91acfebf84e7e25ef = $this->env->getExtension("native_profiler");
+        $__internal_fa49966e1e83f5a176868fbf2fa85e1865ecfed4ea5b0dc91acfebf84e7e25ef->enter($__internal_fa49966e1e83f5a176868fbf2fa85e1865ecfed4ea5b0dc91acfebf84e7e25ef_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "body"));
 
         // line 4
         echo "
@@ -75,68 +75,96 @@ class __TwigTemplate_49939be530bbca10722d1508177345cd4c83d1a9fa808c58c8c537b5989
                 } elseif ((($this->getAttribute($this->getAttribute(                // line 16
 (isset($context["listBoxes"]) ? $context["listBoxes"] : $this->getContext($context, "listBoxes")), 1, array(), "array"), "x", array()) == $context["i"]) && ($this->getAttribute($this->getAttribute((isset($context["listBoxes"]) ? $context["listBoxes"] : $this->getContext($context, "listBoxes")), 1, array(), "array"), "y", array()) == $context["j"]))) {
                     // line 17
-                    echo "                            <image src=\"";
-                    echo twig_escape_filter($this->env, $this->env->getExtension('asset')->getAssetUrl("boxes/snow-s.png"), "html", null, true);
-                    echo "\"></image>
-                        ";
-                } elseif ((($this->getAttribute($this->getAttribute(                // line 18
-(isset($context["listBoxes"]) ? $context["listBoxes"] : $this->getContext($context, "listBoxes")), 2, array(), "array"), "x", array()) == $context["i"]) && ($this->getAttribute($this->getAttribute((isset($context["listBoxes"]) ? $context["listBoxes"] : $this->getContext($context, "listBoxes")), 2, array(), "array"), "y", array()) == $context["j"]))) {
-                    // line 19
-                    echo "                            <image src=\"";
-                    echo twig_escape_filter($this->env, $this->env->getExtension('asset')->getAssetUrl("boxes/snow-m.png"), "html", null, true);
-                    echo "\"></image>
-                        ";
-                } elseif ((($this->getAttribute($this->getAttribute(                // line 20
-(isset($context["listBoxes"]) ? $context["listBoxes"] : $this->getContext($context, "listBoxes")), 3, array(), "array"), "x", array()) == $context["i"]) && ($this->getAttribute($this->getAttribute((isset($context["listBoxes"]) ? $context["listBoxes"] : $this->getContext($context, "listBoxes")), 3, array(), "array"), "y", array()) == $context["j"]))) {
-                    // line 21
+                    echo "                            ";
+                    if ((($this->getAttribute($this->getAttribute((isset($context["listBoxes"]) ? $context["listBoxes"] : $this->getContext($context, "listBoxes")), 3, array(), "array"), "x", array()) == $context["i"]) && ($this->getAttribute($this->getAttribute((isset($context["listBoxes"]) ? $context["listBoxes"] : $this->getContext($context, "listBoxes")), 3, array(), "array"), "y", array()) == $context["j"]))) {
+                        // line 18
+                        echo "                                ";
+                        if ((($this->getAttribute($this->getAttribute((isset($context["listBoxes"]) ? $context["listBoxes"] : $this->getContext($context, "listBoxes")), 2, array(), "array"), "x", array()) == $context["i"]) && ($this->getAttribute($this->getAttribute((isset($context["listBoxes"]) ? $context["listBoxes"] : $this->getContext($context, "listBoxes")), 2, array(), "array"), "y", array()) == $context["j"]))) {
+                            // line 19
+                            echo "                                    <image src=\"";
+                            echo twig_escape_filter($this->env, $this->env->getExtension('asset')->getAssetUrl("boxes/snow-lms.png"), "html", null, true);
+                            echo "\">
+                                ";
+                        }
+                        // line 21
+                        echo "                            ";
+                    } else {
+                        // line 22
+                        echo "                                <image src=\"";
+                        echo twig_escape_filter($this->env, $this->env->getExtension('asset')->getAssetUrl("boxes/snow-s.png"), "html", null, true);
+                        echo "\"></image>
+                            ";
+                    }
+                    // line 24
+                    echo "                        ";
+                } elseif ((($this->getAttribute($this->getAttribute((isset($context["listBoxes"]) ? $context["listBoxes"] : $this->getContext($context, "listBoxes")), 2, array(), "array"), "x", array()) == $context["i"]) && ($this->getAttribute($this->getAttribute((isset($context["listBoxes"]) ? $context["listBoxes"] : $this->getContext($context, "listBoxes")), 2, array(), "array"), "y", array()) == $context["j"]))) {
+                    // line 25
+                    echo "                            ";
+                    if ((($this->getAttribute($this->getAttribute((isset($context["listBoxes"]) ? $context["listBoxes"] : $this->getContext($context, "listBoxes")), 3, array(), "array"), "x", array()) == $context["i"]) && ($this->getAttribute($this->getAttribute((isset($context["listBoxes"]) ? $context["listBoxes"] : $this->getContext($context, "listBoxes")), 3, array(), "array"), "y", array()) == $context["j"]))) {
+                        // line 26
+                        echo "                                <image src=\"";
+                        echo twig_escape_filter($this->env, $this->env->getExtension('asset')->getAssetUrl("boxes/snow-lm.png"), "html", null, true);
+                        echo "\"></image>
+                            ";
+                    } else {
+                        // line 28
+                        echo "                                <image src=\"";
+                        echo twig_escape_filter($this->env, $this->env->getExtension('asset')->getAssetUrl("boxes/snow-m.png"), "html", null, true);
+                        echo "\"></image>
+                            ";
+                    }
+                    // line 30
+                    echo "                        ";
+                } elseif ((($this->getAttribute($this->getAttribute((isset($context["listBoxes"]) ? $context["listBoxes"] : $this->getContext($context, "listBoxes")), 3, array(), "array"), "x", array()) == $context["i"]) && ($this->getAttribute($this->getAttribute((isset($context["listBoxes"]) ? $context["listBoxes"] : $this->getContext($context, "listBoxes")), 3, array(), "array"), "y", array()) == $context["j"]))) {
+                    // line 31
                     echo "                            <image src=\"";
                     echo twig_escape_filter($this->env, $this->env->getExtension('asset')->getAssetUrl("boxes/snow-l.png"), "html", null, true);
                     echo "\"></image>
                         ";
                 } else {
-                    // line 23
+                    // line 33
                     echo "                            <image src=\"";
                     echo twig_escape_filter($this->env, $this->env->getExtension('asset')->getAssetUrl("boxes/grass.png"), "html", null, true);
                     echo "\"></image>
                         ";
                 }
-                // line 25
+                // line 35
                 echo "
                     </li>
                     ";
-                // line 27
+                // line 37
                 $context["n"] = ((isset($context["n"]) ? $context["n"] : $this->getContext($context, "n")) + 1);
-                // line 28
+                // line 38
                 echo "                ";
             }
             $_parent = $context['_parent'];
             unset($context['_seq'], $context['_iterated'], $context['_key'], $context['j'], $context['_parent'], $context['loop']);
             $context = array_intersect_key($context, $_parent) + $_parent;
-            // line 29
+            // line 39
             echo "            </ul>
         ";
         }
         $_parent = $context['_parent'];
         unset($context['_seq'], $context['_iterated'], $context['_key'], $context['i'], $context['_parent'], $context['loop']);
         $context = array_intersect_key($context, $_parent) + $_parent;
-        // line 31
+        // line 41
         echo "    </ul>
     <hr/>
     ";
-        // line 33
-        $this->loadTemplate("snowballBundle:Default:HUD.html.twig", "snowballBundle:Default:index.html.twig", 33)->display($context);
+        // line 43
+        $this->loadTemplate("snowballBundle:Default:HUD.html.twig", "snowballBundle:Default:index.html.twig", 43)->display($context);
         
-        $__internal_dbcb598e1f16a376a033a469bc6fe1f94cf257cb2862e2d2d2e7b06a6993e05e->leave($__internal_dbcb598e1f16a376a033a469bc6fe1f94cf257cb2862e2d2d2e7b06a6993e05e_prof);
+        $__internal_fa49966e1e83f5a176868fbf2fa85e1865ecfed4ea5b0dc91acfebf84e7e25ef->leave($__internal_fa49966e1e83f5a176868fbf2fa85e1865ecfed4ea5b0dc91acfebf84e7e25ef_prof);
 
     }
 
-    // line 36
+    // line 46
     public function block_style($context, array $blocks = array())
     {
-        $__internal_23ff390bade39036b0c5e86b26d7a8045f8f33ed543ffacc5ada649cf7d734a5 = $this->env->getExtension("native_profiler");
-        $__internal_23ff390bade39036b0c5e86b26d7a8045f8f33ed543ffacc5ada649cf7d734a5->enter($__internal_23ff390bade39036b0c5e86b26d7a8045f8f33ed543ffacc5ada649cf7d734a5_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "style"));
+        $__internal_24f8573088bc8d97334570e16dc6ff53aadb5538c0428f336b4fbf063cf93b2f = $this->env->getExtension("native_profiler");
+        $__internal_24f8573088bc8d97334570e16dc6ff53aadb5538c0428f336b4fbf063cf93b2f->enter($__internal_24f8573088bc8d97334570e16dc6ff53aadb5538c0428f336b4fbf063cf93b2f_prof = new Twig_Profiler_Profile($this->getTemplateName(), "block", "style"));
 
-        // line 37
+        // line 47
         echo "    <style>
         ul,li {
             list-style-type : none;
@@ -155,7 +183,7 @@ class __TwigTemplate_49939be530bbca10722d1508177345cd4c83d1a9fa808c58c8c537b5989
     </style>
 ";
         
-        $__internal_23ff390bade39036b0c5e86b26d7a8045f8f33ed543ffacc5ada649cf7d734a5->leave($__internal_23ff390bade39036b0c5e86b26d7a8045f8f33ed543ffacc5ada649cf7d734a5_prof);
+        $__internal_24f8573088bc8d97334570e16dc6ff53aadb5538c0428f336b4fbf063cf93b2f->leave($__internal_24f8573088bc8d97334570e16dc6ff53aadb5538c0428f336b4fbf063cf93b2f_prof);
 
     }
 
@@ -171,7 +199,7 @@ class __TwigTemplate_49939be530bbca10722d1508177345cd4c83d1a9fa808c58c8c537b5989
 
     public function getDebugInfo()
     {
-        return array (  140 => 37,  134 => 36,  127 => 33,  123 => 31,  116 => 29,  110 => 28,  108 => 27,  104 => 25,  98 => 23,  92 => 21,  90 => 20,  85 => 19,  83 => 18,  78 => 17,  76 => 16,  71 => 15,  69 => 14,  65 => 12,  60 => 11,  58 => 10,  55 => 9,  50 => 8,  47 => 7,  45 => 6,  41 => 4,  35 => 3,  11 => 1,);
+        return array (  168 => 47,  162 => 46,  155 => 43,  151 => 41,  144 => 39,  138 => 38,  136 => 37,  132 => 35,  126 => 33,  120 => 31,  117 => 30,  111 => 28,  105 => 26,  102 => 25,  99 => 24,  93 => 22,  90 => 21,  84 => 19,  81 => 18,  78 => 17,  76 => 16,  71 => 15,  69 => 14,  65 => 12,  60 => 11,  58 => 10,  55 => 9,  50 => 8,  47 => 7,  45 => 6,  41 => 4,  35 => 3,  11 => 1,);
     }
 }
 /* {%  extends('::base.html.twig') %}*/
@@ -190,9 +218,19 @@ class __TwigTemplate_49939be530bbca10722d1508177345cd4c83d1a9fa808c58c8c537b5989
 /*                         {% if listBoxes[0].x == i and listBoxes[0].y == j %}*/
 /*                             <image src="{{ asset('boxes/player.png') }}"></image>*/
 /*                         {% elseif listBoxes[1].x == i and listBoxes[1].y == j %}*/
-/*                             <image src="{{ asset('boxes/snow-s.png') }}"></image>*/
+/*                             {% if listBoxes[3].x == i and listBoxes[3].y == j %}*/
+/*                                 {% if listBoxes[2].x == i and listBoxes[2].y == j %}*/
+/*                                     <image src="{{ asset('boxes/snow-lms.png') }}">*/
+/*                                 {% endif %}*/
+/*                             {% else %}*/
+/*                                 <image src="{{ asset('boxes/snow-s.png') }}"></image>*/
+/*                             {% endif %}*/
 /*                         {% elseif listBoxes[2].x == i and listBoxes[2].y == j %}*/
-/*                             <image src="{{ asset('boxes/snow-m.png') }}"></image>*/
+/*                             {% if listBoxes[3].x == i and listBoxes[3].y == j %}*/
+/*                                 <image src="{{ asset('boxes/snow-lm.png') }}"></image>*/
+/*                             {% else %}*/
+/*                                 <image src="{{ asset('boxes/snow-m.png') }}"></image>*/
+/*                             {% endif %}*/
 /*                         {% elseif listBoxes[3].x == i and listBoxes[3].y == j %}*/
 /*                             <image src="{{ asset('boxes/snow-l.png') }}"></image>*/
 /*                         {% else %}*/
